85eabfa1626c13510b15d0ec06cd49ef409696f4
ZoziLaMalice <lorenzo.camus@essca.eu>
2020-04-30 20:21:47 +0200

    app created and it's work!

---
+++ app.py
@@ -0,0 +1,109 @@
+from flask import Flask, render_template, request, url_for, flash, redirect
+from flask import send_from_directory
+
+import os
+import IPython.display as display
+import PIL
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
+import time
+from werkzeug.utils import secure_filename
+
+from model_functions import normalize, get_model, gram_matrix, clip
+from model_functions import StyleContentModel, style_content_loss, tensor_to_image
+
+
+
+app = Flask(__name__)
+
+
+UPLOAD_FOLDER = 'ml_app/uploads'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+OUTPUT_FOLDER = 'ml_app/static/output'
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['OUTPUT_FOLDER'] = OUTPUT_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+optimizer = tf.keras.optimizers.Adam(learning_rate=0.02)
+
+
+@app.route('/', methods=['POST', 'GET'])
+def home():
+    if request.method == 'POST':
+        global epoch
+        epoch = request.form.get('epochs')
+        global steps
+        steps = request.form.get('steps')
+        # check if the post request has the file part
+        if 'upload_file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['upload_file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('styled', filename=filename))
+
+    return render_template('index.html')
+
+
+@app.route('/uploads/<filename>')
+def styled(filename):
+    style_layers = ['block1_conv1', 'block2_conv1', 'block3_conv1', 'block4_conv1', 'block5_conv1']
+    content_layer = ['block5_conv2']
+
+    style_extractor = get_model(style_layers)
+    content_extractor = get_model(content_layer)
+
+    model = StyleContentModel(style_layers=style_layers, content_layer=content_layer)
+
+    style_targets = model(normalize('ml_app/style/kandinsky.jpg'))['style']
+    content_targets = model(normalize(UPLOAD_FOLDER+ "/"+filename))['content']
+    
+    image = tf.Variable(normalize(UPLOAD_FOLDER+ "/"+filename))
+    
+    
+    EPOCHS = int(epoch)
+    steps_per_epoch = int(steps)
+
+    step = 0
+    start = time.time()
+
+    for n in range(EPOCHS):
+        for m in range(steps_per_epoch):
+            step += 1
+            with tf.GradientTape() as tape:
+                outputs = model(image)
+                loss = style_content_loss(outputs, style_targets, content_targets, style_layers, content_layer, 1e-2, 1e4)
+
+            grad = tape.gradient(loss, image)
+            optimizer.apply_gradients([(grad, image)])
+            image.assign(clip(image))
+    
+        print("Train step: {}".format(step))
+    #model.save('my_model.h5')
+
+    tensor_to_image(image).save(OUTPUT_FOLDER+ "/"+filename)
+
+    end = time.time()
+    print("Total time: {:.1f}".format(end-start))
+
+    return render_template("output.html", user_image = filename)
+
+
+if __name__ == "__main__":
+    app.secret_key = 'super secret key'
+    app.config['SESSION_TYPE'] = 'filesystem'
+
+
+    app.run(debug=True)
---
+++ model_functions.py
@@ -0,0 +1,110 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
+import time
+import PIL
+
+
+def normalize(img):
+  max_dim = 512
+
+  img = tf.io.read_file(img)
+  img = tf.image.decode_jpeg(img, channels=3)
+  img = tf.image.convert_image_dtype(img, tf.float64) # Nous fait passer sur une échelle de 0 à 1
+  
+  shape = tf.cast(tf.shape(img)[:-1], tf.float32)
+  long_dim = max(shape)
+  scale = max_dim / long_dim
+
+  new_shape = tf.cast(shape * scale, tf.int32)
+
+  img = tf.image.resize(img, new_shape)
+  img = img[tf.newaxis, :]
+  
+  return img
+
+
+kand_img = normalize('ml_app/style/kandinsky.jpg')
+
+
+def get_model(name):
+  vgg = tf.keras.applications.VGG19(include_top=False)
+  vgg.trainable = False
+
+  model = tf.keras.Model([vgg.input], [vgg.get_layer(x).output for x in name])
+  
+  return model
+
+
+def gram_matrix(input_tensor):
+  
+  result = tf.linalg.einsum('bijc,bijd->bcd', input_tensor, input_tensor)
+  
+  input_shape = tf.shape(input_tensor)
+  num_locations = tf.cast(input_shape[1]*input_shape[2], tf.float32)
+  
+  return result/(num_locations)
+
+
+def clip(img):
+  return tf.clip_by_value(img, clip_value_min=0.0, clip_value_max=1.0)
+
+
+class StyleContentModel(tf.keras.models.Model):
+  def __init__(self, style_layers, content_layer):
+    super(StyleContentModel, self).__init__()
+    
+    self.vgg = get_model(style_layers + content_layer)
+    
+    self.style_layers = style_layers
+    self.content_layers = content_layer
+    
+    self.num_style_layers = len(style_layers)
+    
+    self.vgg.trainable = False
+
+
+  def __call__(self, inputs):
+    
+    inputs = tf.keras.applications.vgg19.preprocess_input(inputs*255)
+    inputs = tf.image.resize(inputs, (224, 224))
+    preds = self.vgg(inputs)
+    
+    style_outputs = preds[:self.num_style_layers]
+    content_outputs = preds[self.num_style_layers:]
+    
+    style_outputs = [gram_matrix(style_output) for style_output in style_outputs]
+    
+    style_dict = {}
+    content_dict = {}
+    
+    for (key, item) in zip(self.style_layers, style_outputs):
+      style_dict[key] = item
+    
+    for (key, item) in zip(self.content_layers, content_outputs):
+      content_dict[key] = item
+    
+    return {'content': content_dict, 'style': style_dict}
+
+
+def style_content_loss(outputs, style_targets, content_targets, style_layers, content_layer, style_weight=1e-2, content_weight=1e-4):
+  style_outputs = outputs['style']
+  content_outputs = outputs['content']
+
+  style_loss = tf.add_n([tf.reduce_mean((style_outputs[name]-style_targets[name])**2) 
+                          for name in style_outputs.keys()])
+  style_loss *= style_weight / len(style_layers)
+
+  content_loss = tf.add_n([tf.reduce_mean((content_outputs[name]-content_targets[name])**2) 
+                            for name in content_outputs.keys()])
+  content_loss *= content_weight / len(content_layer)
+
+  return content_loss + style_loss
+
+def tensor_to_image(tensor):
+  tensor = tensor*255
+  tensor = np.array(tensor, dtype=np.uint8)
+  if np.ndim(tensor)>3:
+    assert tensor.shape[0] == 1
+    tensor = tensor[0]
+  return PIL.Image.fromarray(tensor)
